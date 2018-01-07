60e8f46b96b58f68731f1a5d1c7d24d127f562d5
Daniel Savage <dansvg@gmail.com>
2018-01-06 23:25:32 -0800

    Hello, world!

---
+++ src/gameplay.js
@@ -0,0 +1,12 @@
+var Gameplay = function () {
+  // body...
+};
+Gameplay.prototype.create = function() {
+  this.game.add.bitmapText(32, 32, 'font', 'transform!', 8);
+};
+Gameplay.prototype.update = function() {
+  //
+};
+Gameplay.prototype.shutdown = function() {
+  //
+};
---
+++ src/main.js
@@ -0,0 +1,43 @@
+var Preload = function () {
+	//
+};
+Preload.prototype.init = function() {
+  this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  this.game.scale.refresh();
+
+  this.game.scale.pageAlignHorizontally = true;
+  this.game.scale.pageAlignVertically = true;
+
+  // enable crisp rendering
+  this.game.stage.smoothed = false;
+  this.game.renderer.renderSession.roundPixels = true;  
+  Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+  PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST; //for WebGL
+
+  this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.DOWN);
+  this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.UP);
+  this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.SPACEBAR);
+
+  this.game.input.gamepad.start();
+};
+Preload.prototype.preload = function() {
+  // Font is Gamegirl Classic by freakyfonts
+  // License is for noncommercial use
+  // http://www.fontspace.com/freaky-fonts/gamegirl-classic
+  this.game.load.bitmapFont('font', 'asset/font/font.png', 'asset/font/font.json');
+};
+Preload.prototype.create = function() {
+  this.game.state.start('Gameplay');
+};
+
+
+
+var main = function () {
+	console.log('hello, jam! 😊');
+
+	var game = new Phaser.Game(640, 480);
+	game.state.add('Preload', Preload, false);
+  game.state.add('Gameplay', Gameplay, false);
+
+	game.state.start('Preload');
+};
