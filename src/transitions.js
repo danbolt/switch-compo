47aa1f43b0785080c21ef4a6fb7a4d2cb164d82b
Daniel Savage <dansvg@gmail.com>
2018-02-08 11:45:03 -0800

    Add first spectre encounter

--- src/transitions.js
+++ src/transitions.js
@@ -3,4 +3,5 @@ var TransitionTable = {
   "map2map3": "map2 to map1",
   "map3map1": "map3 to map1",
   "first_roomsecond_room": "the first piece of dialogue",
+  "second_roomenter_ruins": "enter the ruins",
 };
