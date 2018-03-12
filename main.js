389f6ae3c620ad17d7b46269aa500de00f887982
Daniel Savage <dansvg@gmail.com>
2018-03-11 19:55:12 -0700

    Add window app cosmetics

--- main.js
+++ main.js
@@ -1,9 +1,56 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu, MenuItem, globalShortcut, dialog} = require('electron');
+const nativeImage = require('electron').nativeImage;
 const url = require('url');
 const path = require('path');
 
 let createWindow = function() {
-  let win = new BrowserWindow({width: 800, height: 600});
+  let winIcon = nativeImage.createFromPath('icon.png');
+  let win = new BrowserWindow({width: 800, height: 600, icon: winIcon});
+  win.setAutoHideMenuBar(true);
+  win.setMenuBarVisibility(false);
+
+  const menuTemplate = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Quit',
+          click: () => {
+            app.quit();
+          }
+        }
+      ]
+    },
+    {
+      label: 'Options',
+      submenu: [
+        {
+          label: 'Toggle Fullscreen',
+          sublabel: 'Alt-Enter',
+          click: () => {
+            win.setFullScreen(!(win.isFullScreen()));
+          }
+        }
+      ]
+    },
+    {
+      label: 'Help',
+      submenu: [
+          {
+              label: 'About ...',
+              click: () => {
+                  dialog.showMessageBox(win, { butons: ['OK'], icon: winIcon, title: 'Spectres of the Cold', message: '©Copyright 2018\nDaniel Savage and Jesse Taylor\n\nhttps://danbolt.itch.io/' });
+              }
+          }
+      ]
+    },
+  ];
+  let topMenu = Menu.buildFromTemplate(menuTemplate);
+  win.setMenu(topMenu);
+
+  globalShortcut.register('Alt+Enter', () => {
+            win.setFullScreen(!(win.isFullScreen()));
+          });
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
