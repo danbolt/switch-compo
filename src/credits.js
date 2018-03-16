7fdba4f60d7a09776f41c03b6ccce5c36ce94ed4
Daniel Savage <dansvg@gmail.com>
2018-03-15 22:02:49 -0700

    Update credits list

--- src/credits.js
+++ src/credits.js
@@ -6,6 +6,7 @@ var Credits = function () {
     { title: "Playtesting", who: ["Natasha Miner", "Dylan Shellenberg", "Rose-Lynne Savage", "Jesse Taylor", "Clark Allenby", "Wilson Chung"] },
     { title: "Development Support", who: "Jesse Taylor" },
     { title: "Audio", who: ["noiseforfun.com", "SocializedArtist45", "spookymodem", "TobiasKosmos", "Daniel Savage", "XxChr0nosxX", "Aleks41", "qubodup", "Erokia", "xDimebagx", "jorge0000"] },
+    { title: "Localization", who: ["Alina Varela - Español"] },
     { title: "Special Thanks", who: ["Byron Munsie", "Alina Varela", "Geoff Fisher", "Alyssa Savage", "Richard \"photonstorm\" Davey", "Mom and Dad"] },
     { title: "danbolt.itch.io", click: "https://danbolt.itch.io" }
   ];
